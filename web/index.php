8916569299018211f362a78b124c1655c8f432db
AHDIDOU <ahdidou@mobiblanc.com>
2014-02-25 16:42:33 +0000

    changed somthing

--- web/index.php
+++ web/index.php
@@ -1,8 +1,10 @@
 <?php
 
-echo "<h1>Hello There i am from GIT 111111</h1>";
+
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
+
+echo "<h1>Hello There i am from JENKINS</h1>";
