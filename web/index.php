f7974c97b8e381bfc9bb8cfa2ff4fff129d2ca8f
AHDIDOU <ahdidou@mobiblanc.com>
2014-02-26 17:02:02 +0000

    Mehdi test

--- web/index.php
+++ web/index.php
@@ -7,6 +7,10 @@ require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
 
+
 echo "<h1>Hello There i am from JENKINS</h1>";
 
 echo "<h1>Hello There i am from JENKINS</h1>";
+
+echo "<h1>Hello There i am from JENKINS</h1>";
+
